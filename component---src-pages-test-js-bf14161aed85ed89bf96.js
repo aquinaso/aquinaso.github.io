08fe1c51a4a81f1fc1444e28cb99e0b7bd27ebe2
LeeYoungChan <yclee@inventis.co.kr>
2023-07-19 16:44:25 +0900

    Updates

---
+++ component---src-pages-test-js-bf14161aed85ed89bf96.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_gatsby_site=self.webpackChunkmy_gatsby_site||[]).push([[672],{206:function(e,t,l){l.r(t),l.d(t,{Head:function(){return u}});var n=l(7294);const u=()=>n.createElement("title",null,"TEST PAGE");t.default=()=>n.createElement("main",null,n.createElement("h1",null,"About Me"),n.createElement("p",null,"Hi there! I'm the proud creator of this site, which I built with Gatsby."))}}]);
+//# sourceMappingURL=component---src-pages-test-js-bf14161aed85ed89bf96.js.map
