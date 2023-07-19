834064bd23918ba0d24689e5710196ea1e8611a3
LeeYoungChan <yclee@inventis.co.kr>
2023-07-19 15:43:55 +0900

    my new gatsby site

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,21 +4,36 @@
 module.exports = {
   siteMetadata: {
     title: `My Gatsby Site`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://my-gatsby-site.aquinaso.io`
   },
-  plugins: ["gatsby-plugin-google-gtag", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
+  plugins: ["gatsby-plugin-image", "gatsby-plugin-sitemap",
+  {
+    resolve: "gatsby-plugin-google-gtag",
+    options: {
+      trackingIds: [
+        "G-73Y5H7SC2Y"
+      ], 
+      gtagConfig: {
+        ganonymize_ip: true, 
+      },
+    },
+  },
+  {
     resolve: 'gatsby-plugin-manifest',
     options: {
       "icon": "src/images/icon.png"
     }
-  }, "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+  },
+  "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp",
+  {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "images",
       "path": "./src/images/"
     },
     __key: "images"
-  }, {
+  },
+  {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "pages",
