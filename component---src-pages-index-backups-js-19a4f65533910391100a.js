2844fe9118d2af6df3d1f378854ffec3cb95aff3
LeeYoungChan <yclee@inventis.co.kr>
2023-07-21 11:20:52 +0900

    Updates

---
+++ component---src-pages-index-backups-js-19a4f65533910391100a.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_gatsby_site=self.webpackChunkmy_gatsby_site||[]).push([[664],{8235:function(e,t,l){l.r(t),l.d(t,{Head:function(){return u}});var n=l(7294),a=l(1883);const u=()=>n.createElement("title",null,"Home Page");t.default=()=>n.createElement("main",null,n.createElement("h1",null,"Welcome to my Gatsby site!"),n.createElement(a.Link,{to:"/about"},"About"),n.createElement("p",null,"I'm making this by following the Gatsby Tutorial."))}}]);
+//# sourceMappingURL=component---src-pages-index-backups-js-19a4f65533910391100a.js.map
---
+++ component---src-pages-index-tsx-d5daf01f912255981016.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_gatsby_site=self.webpackChunkmy_gatsby_site||[]).push([[691],{4276:function(e,t,n){n.r(t),n.d(t,{default:function(){return a}});var r=n(7294);var u=function(e){let{text:t}=e;return r.createElement("div",null,t)};var a=function(){return r.createElement(u,{text:"Home"})}}}]);
+//# sourceMappingURL=component---src-pages-index-tsx-d5daf01f912255981016.js.map
